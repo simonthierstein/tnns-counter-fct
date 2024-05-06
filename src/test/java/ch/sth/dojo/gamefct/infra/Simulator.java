88fe2438bdf93fe7c4c5a46c741ca62146c03e1c
u203243 <simon.thierstein@sbb.ch>
2024-05-06 17:12:23 +0200

    es

--- src/test/java/ch/sth/dojo/gamefct/infra/Simulator.java
+++ src/test/java/ch/sth/dojo/gamefct/infra/Simulator.java
@@ -5,8 +5,8 @@
 package ch.sth.dojo.gamefct.infra;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.condition.MappedCondition.mappedCondition;
 
-import ch.sth.dojo.es.AbgeschlossenesGame;
 import ch.sth.dojo.es.Game;
 import ch.sth.dojo.es.GameAggregateRoot;
 import ch.sth.dojo.es.LaufendesGame;
@@ -15,9 +15,14 @@ import ch.sth.dojo.es.events.DomainEvent;
 import ch.sth.dojo.es.events.GameErzeugt;
 import ch.sth.dojo.es.events.SpielerHatPunktGewonnen;
 import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.control.Option;
 import java.util.function.Function;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
+import org.assertj.core.condition.MappedCondition;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.jupiter.api.Test;
 
 public class Simulator {
@@ -36,26 +41,36 @@ public class Simulator {
         Function<PreInitializedGame, DomainEvent> erzeuge = x -> GameAggregateRoot.erzeugeGame();
         Function<LaufendesGame, DomainEvent> spieler = x -> GameAggregateRoot.spielerPunktet(x);
         Function<LaufendesGame, DomainEvent> gegner = x -> GameAggregateRoot.gegnerPunktet(x);
-        Function<AbgeschlossenesGame, DomainEvent> errAbg = x -> {
-            throw new RuntimeException(String.format("invalid command for state=%1", x));
-        };
-        Function<PreInitializedGame, DomainEvent> errPre = x -> {
-            throw new RuntimeException(String.format("invalid command for state=%1", x));
-        };
 
-
-        final DomainEvent orElseThrow = Option.some(GameAggregateRoot.empty())
+        var orElseThrow = Option.some(GameAggregateRoot.empty())
                 .map(state -> Tuple.of(state, erzeuge.apply(state)))
-                .map(t2 -> t2.map2(domainEvent -> GameAggregateRoot.handleEvent(t2._1, domainEvent))._2)
-                .map(state -> Game.apply(state,
-                        spieler,
-                        errAbg,
-                        errPre))
+                .map(stateEvent2State())
+                .map(state2StateEvent(spieler))
+                .map(stateEvent2State())
+                .map(state2StateEvent(spieler))
                 .getOrElseThrow(() -> new RuntimeException());
 
 
-        assertThat(orElseThrow).isInstanceOf(SpielerHatPunktGewonnen.class);
+        assertThat(orElseThrow._2).isInstanceOf(SpielerHatPunktGewonnen.class);
 
+    }
+
+    private static Function<Game, Tuple2<Game, DomainEvent>> state2StateEvent(final Function<LaufendesGame, DomainEvent> spieler) {
+        return state -> Tuple.of(state, Game.apply(state,
+                spieler,
+                err(),
+                err()));
+    }
+
+    private static Function<Tuple2<? extends Game, DomainEvent>, Game> stateEvent2State() {
+        return t2 -> t2.map2(domainEvent -> GameAggregateRoot.handleEvent(t2._1, domainEvent))._2;
+    }
+
+
+    static <I> Function<I, DomainEvent> err() {
+        return i -> {
+            throw new RuntimeException(String.format("invalid command for state=%s", i));
+        };
     }
 }
 
