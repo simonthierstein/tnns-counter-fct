2a525ea9f428eb6a82eca471bdd3db870ebde972
u203243 <simon.thierstein@sbb.ch>
2024-05-09 13:17:03 +0200

    errorhandling

--- src/test/java/ch/sth/dojo/gamefct/infra/Simulator.java
+++ src/test/java/ch/sth/dojo/gamefct/infra/Simulator.java
@@ -9,42 +9,55 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ch.sth.dojo.es.Game;
 import ch.sth.dojo.es.GameAggregateRoot;
 import ch.sth.dojo.es.LaufendesGame;
+import ch.sth.dojo.es.PreInitializedGame;
 import ch.sth.dojo.es.commands.GegnerPunktet;
 import ch.sth.dojo.es.commands.SpielerPunktet;
 import ch.sth.dojo.es.events.DomainEvent;
-import ch.sth.dojo.es.events.SpielerHatGameGewonnen;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import io.vavr.control.Either;
-import io.vavr.control.Option;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import org.junit.jupiter.api.Test;
 
 class Simulator {
 
     @Test
     void name() {
-        var orElseThrow = Option.some(Game.empty())
+        Either<DomainError, Tuple2<Game, DomainEvent>> orElseThrow = Either.<DomainError, PreInitializedGame>right(Game.empty())
                 .map(state -> Tuple.of(state, state).map2(GameAggregateRoot.erzeugeSpiel()))
-                .map(unwrap(doSpielerPunktet()))
-                .map(unwrap(doSpielerPunktet()))
-                .map(unwrap(doGegnerPunktet()))
-                .map(unwrap(doSpielerPunktet()))
-                .map(unwrap(doSpielerPunktet()))
-                .map(unwrap(doSpielerPunktet()))
-                .getOrElseThrow(RuntimeException::new);
+                .flatMap(doSpielerPunktet())
+                .flatMap(doSpielerPunktet())
+                .flatMap(doSpielerPunktet())
+                .flatMap(doSpielerPunktet())
+                .flatMap(doSpielerPunktet())
+                .peekLeft(System.err::println);
 
 
-        assertThat(orElseThrow._2).isInstanceOf(SpielerHatGameGewonnen.class);
+        assertThat(passIfeitherLeftPred()).accepts(orElseThrow);
+        assertThat(Predicate.not(passIfeitherRightPred())).accepts(orElseThrow);
 
     }
 
-    private static Function<Tuple2<? extends Game, DomainEvent>, Tuple2<Game, DomainEvent>> unwrap(final Function<Tuple2<? extends Game, DomainEvent>, Tuple2<Game, Either<DomainError, DomainEvent>>> commandFct) {
-        return commandFct.andThen(gameEitherTuple2 -> gameEitherTuple2.map2(eith->eith.getOrElseThrow(err->new RuntimeException(err.toString()))));
+    private static Predicate<Either<?, ?>> passIfeitherLeftPred() {
+        return Either::isLeft;
     }
 
-    private static Function<Tuple2<? extends Game, DomainEvent>, Tuple2<Game, Either<DomainError, DomainEvent>>> doSpielerPunktet() {
-        return stateEvent2State().andThen(command2StateEvent(GameAggregateRoot.command(new SpielerPunktet())));
+    private static Predicate<Either<?, ?>> passIfeitherRightPred() {
+        return Either::isRight;
+    }
+
+    private static Function<Tuple2<? extends Game, DomainEvent>, Tuple2<Game, DomainEvent>> unwrap(final Function<Tuple2<? extends Game, DomainEvent>,
+            Tuple2<Game, Either<DomainError, DomainEvent>>> commandFct) {
+        return commandFct.andThen(gameEitherTuple2 -> gameEitherTuple2.map2(eith -> eith.getOrElseThrow(err -> new RuntimeException(err.toString()))));
+    }
+
+    private static Function<Tuple2<? extends Game, DomainEvent>, Either<DomainError, Tuple2<Game, DomainEvent>>> doSpielerPunktet() {
+        return stateEvent2State().andThen(command2StateEvent(GameAggregateRoot.command(new SpielerPunktet()))).andThen(transform());
+    }
+
+    private static Function<? super Tuple2<Game, Either<DomainError, DomainEvent>>, Either<DomainError, Tuple2<Game, DomainEvent>>> transform() {
+        return prev -> prev._2.map(domainEvent -> Tuple.of(prev._1, domainEvent));
     }
 
     private static Function<Tuple2<? extends Game, DomainEvent>, Tuple2<Game, Either<DomainError, DomainEvent>>> doGegnerPunktet() {
