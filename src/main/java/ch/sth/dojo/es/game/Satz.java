7746ec8fbc5617bca96f91672a6840476eb99480
u203243 <simon.thierstein@sbb.ch>
2024-05-22 16:42:58 +0200

    satz

---
+++ src/main/java/ch/sth/dojo/es/game/Satz.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2024.
+ */
+
+package ch.sth.dojo.es.game;
+
+public interface Satz {
+    static Satz spielerHatPunktGewonnen() {
+        return null;
+    }
+}
---
+++ src/test/java/ch/sth/dojo/es/game/SatzTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2024.
+ */
+
+package ch.sth.dojo.es.game;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SatzTest {
+    @Test
+    void fdsafdsa() {
+        assertThat(Satz.spielerHatPunktGewonnen()).isNotNull();
+    }
+}
