cc22890b736bded16e89d6e0ee146de5e99354a2
u203243 <simon.thierstein@sbb.ch>
2024-06-02 10:09:59 +0200

    StandardMatch

--- src/main/java/ch/sth/dojo/es/match/cmd/MatchCommandHandler.java
+++ src/main/java/ch/sth/dojo/es/match/cmd/MatchCommandHandler.java
@@ -6,24 +6,56 @@ import ch.sth.dojo.es.events.GegnerHatMatchGewonnen;
 import ch.sth.dojo.es.events.GegnerHatSatzGewonnen;
 import ch.sth.dojo.es.events.SpielerHatMatchGewonnen;
 import ch.sth.dojo.es.events.SpielerHatSatzGewonnen;
+import ch.sth.dojo.es.match.AbgeschlossenesStandardMatch;
 import ch.sth.dojo.es.match.LaufendesStandardMatch;
 import ch.sth.dojo.es.match.StandardMatch;
 import io.vavr.control.Either;
+import java.util.function.Function;
 
-public interface MatchCommandHandler {
+public final class MatchCommandHandler {
 
-    static Either<DomainError, DomainEvent> spielerGewinneSatzCommand(LaufendesStandardMatch state) {
-        return LaufendesStandardMatch.incrementSpieler(state)
-                .map(next -> StandardMatch.apply(next,
-                        laufendesStandardMatch -> new SpielerHatSatzGewonnen(),
-                        abgeschlossenesStandardMatch -> new SpielerHatMatchGewonnen()));
+    static Either<DomainError, DomainEvent> spielerGewinneSatzCommand(StandardMatch state) {
+        return StandardMatch.apply(state,
+                spielerGewinneSatz(),
+                invalidStateForMatch()
+        );
     }
 
-    static Either<DomainError, DomainEvent> gegnerGewinneSatzCommand(LaufendesStandardMatch state) {
-        return LaufendesStandardMatch.incrementGegner(state)
-                .map(next -> StandardMatch.apply(next,
-                        laufendesStandardMatch -> new GegnerHatSatzGewonnen(),
-                        abgeschlossenesStandardMatch -> new GegnerHatMatchGewonnen()));
+    static Either<DomainError, DomainEvent> gegnerGewinneSatzCommand(StandardMatch state) {
+        return StandardMatch.apply(state,
+                gegnerGewinneSatz(),
+                invalidStateForMatch()
+        );
     }
+
+    private static Function<AbgeschlossenesStandardMatch, Either<DomainError, DomainEvent>> invalidStateForMatch() {
+        return asm -> Either.left(new DomainError.InvalidStateForMatch());
+    }
+
+    private static Function<LaufendesStandardMatch, Either<DomainError, DomainEvent>> spielerGewinneSatz() {
+        return lsm -> LaufendesStandardMatch.incrementSpieler(lsm)
+                .map(next -> StandardMatch.apply(next,
+                        laufendesMatch2SpielerSatzGewonnen,
+                        laufendesMatch2SpielerMatchGewonnen));
+    }
+
+    private static Function<LaufendesStandardMatch, Either<DomainError, DomainEvent>> gegnerGewinneSatz() {
+        return lsm -> LaufendesStandardMatch.incrementGegner(lsm)
+                .map(next -> StandardMatch.apply(next,
+                        laufendesStandardMatch2GegnerSatzGewonnen,
+                        abgeschlossenesStandardMatch2GegnerMatchGewonnen));
+    }
+
+    private static final Function<AbgeschlossenesStandardMatch, DomainEvent> abgeschlossenesStandardMatch2GegnerMatchGewonnen =
+            abgeschlossenesStandardMatch -> new GegnerHatMatchGewonnen();
+
+    private static final Function<LaufendesStandardMatch, DomainEvent> laufendesStandardMatch2GegnerSatzGewonnen =
+            laufendesStandardMatch -> new GegnerHatSatzGewonnen();
+
+    private static final Function<LaufendesStandardMatch, DomainEvent> laufendesMatch2SpielerSatzGewonnen =
+            laufendesStandardMatch -> new SpielerHatSatzGewonnen();
+
+    private static final Function<AbgeschlossenesStandardMatch, DomainEvent> laufendesMatch2SpielerMatchGewonnen =
+            abgeschlossenesStandardMatch -> new SpielerHatMatchGewonnen();
 
 }
---
+++ src/test/java/ch/sth/dojo/es/match/cmd/MatchCommandHandlerTest.java
@@ -0,0 +1,53 @@
+package ch.sth.dojo.es.match.cmd;
+
+import static ch.sth.dojo.es.match.LaufendesStandardMatch.LaufendesStandardMatch;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import ch.sth.dojo.es.DomainError;
+import ch.sth.dojo.es.events.DomainEvent;
+import ch.sth.dojo.es.match.AbgeschlossenesStandardMatch;
+import ch.sth.dojo.es.match.LaufendesStandardMatch;
+import ch.sth.dojo.es.match.Punkte;
+import ch.sth.dojo.es.match.PunkteGegner;
+import ch.sth.dojo.es.match.PunkteSpieler;
+import ch.sth.dojo.es.match.StandardMatch;
+import ch.sth.dojo.es.match.evt.MatchEventHandler;
+import io.vavr.Tuple;
+import io.vavr.collection.List;
+import io.vavr.control.Either;
+import org.junit.jupiter.api.Test;
+
+class MatchCommandHandlerTest {
+
+    @Test
+    void spielerGewinneSatzCommand() {
+        final LaufendesStandardMatch state = LaufendesStandardMatch(new PunkteSpieler(Punkte.punkte(List.empty())),
+                new PunkteGegner(Punkte.punkte(List.empty()))).get();
+        final Either<DomainError, DomainEvent> domainEvents = MatchCommandHandler.spielerGewinneSatzCommand(state);
+
+        final Either<DomainError, StandardMatch> eith = domainEvents
+                .flatMap(event1 -> MatchEventHandler.handleEvent(state, event1))
+                .flatMap(state1 -> MatchCommandHandler.spielerGewinneSatzCommand(state1)
+                        .map(x -> Tuple.of(state1, x)))
+                .flatMap(t2 -> t2.apply(MatchEventHandler::handleEvent));
+
+        assertThat(eith.isRight()).isTrue();
+        assertThat(eith.get()).isInstanceOf(AbgeschlossenesStandardMatch.class);
+    }
+
+    @Test
+    void gegnerGewinneSatzCommand() {
+        final LaufendesStandardMatch state = LaufendesStandardMatch(new PunkteSpieler(Punkte.punkte(List.empty())),
+                new PunkteGegner(Punkte.punkte(List.empty()))).get();
+        final Either<DomainError, DomainEvent> domainEvents = MatchCommandHandler.spielerGewinneSatzCommand(state);
+
+        final Either<DomainError, StandardMatch> eith = domainEvents
+                .flatMap(event1 -> MatchEventHandler.handleEvent(state, event1))
+                .flatMap(state1 -> MatchCommandHandler.gegnerGewinneSatzCommand(state1)
+                        .map(x -> Tuple.of(state1, x)))
+                .flatMap(t2 -> t2.apply(MatchEventHandler::handleEvent));
+
+        assertThat(eith.isRight()).isTrue();
+        assertThat(eith.get()).isInstanceOf(AbgeschlossenesStandardMatch.class);
+    }
+}
