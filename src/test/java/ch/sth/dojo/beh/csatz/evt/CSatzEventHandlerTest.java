f6691c00f4619603c5f3bf9fdc23b38911767e67
u203243 <simon.thierstein@sbb.ch>
2025-02-15 15:19:57 +0100

    Satz Event handler Gegner

--- src/test/java/ch/sth/dojo/beh/csatz/evt/CSatzEventHandlerTest.java
+++ src/test/java/ch/sth/dojo/beh/csatz/evt/CSatzEventHandlerTest.java
@@ -16,48 +16,29 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class CSatzEventHandlerTest {
 
     @DisplayName("Gamewin Scoring 😎")
-    @ParameterizedTest(name = "Satz handling spieler gewinnt game {0} - {1}")
-    @CsvSource({
-        "0,4", "0,3", "0,2", "0,1", "0,0",
-        "1,4", "1,3", "1,2", "1,1", "1,0",
-        "2,4", "2,3", "2,2", "2,1", "2,0",
-        "3,4", "3,3", "3,2", "3,1", "3,0",
-        "4,4", "4,3", "4,2", "4,1", "4,0"
-    })
-    void handleSpielerGameGewonnenEvent(Integer left, Integer right) {
-        var prev = new LaufenderCSatz(new SpielerPunkteSatz(left), new GegnerPunkteSatz(right), LaufendesCGame.zero());
-        var fdsa = CSatzEventHandler.handleEvent(prev, new SpielerGameGewonnen());
-
-        assertThat(fdsa.isRight()).isTrue();
-    }
-
-    @DisplayName("Gamewin Scoring 😎")
-    @ParameterizedTest(name = "Satz handling spieler gewinnt game {0} - {1}")
-    @CsvSource({
-        "0,4", "0,3", "0,2", "0,1", "0,0",
-        "1,4", "1,3", "1,2", "1,1", "1,0",
-        "2,4", "2,3", "2,2", "2,1", "2,0",
-        "3,4", "3,3", "3,2", "3,1", "3,0",
-        "4,4", "4,3", "4,2", "4,1", "4,0"
-    })
+    @ParameterizedTest
+    @ArgumentsSource(SatzScoreProvider.class)
     void handleGegnerGameGewonnenEvent(Integer left, Integer right) {
-        var prev = new LaufenderCSatz(new SpielerPunkteSatz(right), new GegnerPunkteSatz(left), LaufendesCGame.zero());
-        var fdsa = CSatzEventHandler.handleEvent(prev, new GegnerGameGewonnen());
+        var prev = new LaufenderCSatz(new SpielerPunkteSatz(left), new GegnerPunkteSatz(right), LaufendesCGame.zero());
+        var res = CSatzEventHandler.handleEvent(prev, new GegnerGameGewonnen());
 
-        assertThat(fdsa.isRight()).isTrue();
+        assertThat(res.isRight()).isTrue();
+
     }
 
     @DisplayName("Gamewin Scoring 😎")
     @ParameterizedTest
     @ArgumentsSource(SatzScoreProvider.class)
-    void handleEventfds(Integer left, Integer right) {
+    void handleSpielerGameGewonnenEvent(Integer left, Integer right) {
         var prev = new LaufenderCSatz(new SpielerPunkteSatz(left), new GegnerPunkteSatz(right), LaufendesCGame.zero());
-        var fdsa = CSatzEventHandler.handleEvent(prev, new SpielerGameGewonnen());
+        var res = CSatzEventHandler.handleEvent(prev, new SpielerGameGewonnen());
+
+        assertThat(res.isRight()).isTrue();
+
     }
 
 }
@@ -66,12 +47,8 @@ class SatzScoreProvider implements ArgumentsProvider {
 
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
-        final List<Integer> range = List.range(0, 25);
-
-        //        range.forEach(System.out::println);
-
-        return Stream.of(
-            Arguments.of(1, 1)
-        );
+        final List<Integer> toFiveRange = List.range(0, 5);
+        return toFiveRange
+            .flatMap(i -> toFiveRange.map(j -> Arguments.of(i, j))).toJavaStream();
     }
 }
