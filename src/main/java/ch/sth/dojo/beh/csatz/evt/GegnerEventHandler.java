8f28a7460fba466d40d23f2c7f7f91620e88b820
u203243 <simon.thierstein@sbb.ch>
2025-02-16 17:23:05 +0100

    ignore sub events

--- src/main/java/ch/sth/dojo/beh/csatz/evt/GegnerEventHandler.java
+++ src/main/java/ch/sth/dojo/beh/csatz/evt/GegnerEventHandler.java
@@ -8,8 +8,6 @@ import static io.vavr.control.Either.left;
 import static io.vavr.control.Either.right;
 
 import ch.sth.dojo.beh.DomainProblem;
-import ch.sth.dojo.beh.cgame.domain.CGame;
-import ch.sth.dojo.beh.cgame.evt.CGameEventHandler;
 import ch.sth.dojo.beh.csatz.domain.AbgeschlossenerCSatz;
 import ch.sth.dojo.beh.csatz.domain.CSatz;
 import ch.sth.dojo.beh.csatz.domain.GegnerPunkteSatz;
@@ -17,18 +15,16 @@ import ch.sth.dojo.beh.csatz.domain.LaufenderCSatz;
 import ch.sth.dojo.beh.csatz.domain.SpielerPunkteSatz;
 import ch.sth.dojo.beh.evt.GegnerDomainEvent;
 import ch.sth.dojo.beh.evt.GegnerGameGewonnen;
-import ch.sth.dojo.beh.evt.GegnerPunktGewonnen;
 import ch.sth.dojo.beh.evt.GegnerSatzGewonnen;
 import io.vavr.control.Either;
-import java.util.function.Function;
 
 interface GegnerEventHandler {
 
     static Either<DomainProblem, CSatz> handleGegnerEvent(LaufenderCSatz state, GegnerDomainEvent event) {
         return switch (event) {
             case GegnerGameGewonnen evt -> right(handleEvent(state, evt));
-            case GegnerPunktGewonnen evt -> handleEvent(state, evt);
             case GegnerSatzGewonnen evt -> right(handleEvent(state, evt));
+            default -> left(DomainProblem.eventNotValid);
         };
     }
 
@@ -39,18 +35,6 @@ interface GegnerEventHandler {
     static CSatz handleEvent(LaufenderCSatz state, GegnerGameGewonnen event) {
         return new LaufenderCSatz(new SpielerPunkteSatz(state.spielerPunkteSatz().value()),
             new GegnerPunkteSatz(state.gegnerPunkteSatz().value() + 1), state.currentGame());
-    }
-
-    static Either<DomainProblem, CSatz> handleEvent(LaufenderCSatz state, GegnerPunktGewonnen event) {
-        return CGameEventHandler.handleEvent(state.currentGame(), event)
-            .flatMap(nextLaufenderSatz(state));
-    }
-
-    static Function<CGame, Either<DomainProblem, CSatz>> nextLaufenderSatz(LaufenderCSatz prev) {
-        return cgame -> cgame.apply(
-            laufendesCGame -> right(new LaufenderCSatz(prev.spielerPunkteSatz(), prev.gegnerPunkteSatz(), laufendesCGame)),
-            x -> left(DomainProblem.invalidEvent)
-        );
     }
 
 }
