e2343ca96664d50e78fc8236dc60d156c19b99b8
u203243 <simon.thierstein@sbb.ch>
2025-02-22 10:09:10 +0100

    scenario 1

--- src/test/java/ch/sth/dojo/beh/cgame/cmd/ScenarioTest.java
+++ src/test/java/ch/sth/dojo/beh/cgame/cmd/ScenarioTest.java
@@ -1,5 +1,8 @@
 package ch.sth.dojo.beh.cgame.cmd;
 
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.Match;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -21,6 +24,7 @@ import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.control.Either;
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -32,13 +36,25 @@ class ScenarioTest {
     void spielerGewinntGame_standard(String other) {
 
         final Tuple2<CSatz, CGame> prev = SatzGameTupleWith();
-        final DomainCommand command = DomainCommandWith();
+        final DomainCommand gegnerPunktetCommand = GegnerPunktetWith();
+
+        final Either<DomainProblem, Tuple2<CSatz, CGame>> gegnerPointsApplied = List.of(other)
+            .flatMap(str -> Match(str).of(
+                Case($("00"), List.empty()),
+                Case($("15"), List.range(0, 1)),
+                Case($("30"), List.range(0, 2))
+            ))
+            .map(x -> gegnerPunktetStateTransfer(gegnerPunktetCommand))
+            .foldLeft(Either.right(prev), Either::flatMap);
+
+        final DomainCommand spielerPunkteCommand = SpielerPunktetWith();
+
 
         final Either<DomainProblem, Tuple2<CSatz, CGame>> result = List.range(0, 4)
-            .foldLeft(Either.right(prev),
+            .foldLeft(gegnerPointsApplied,
                 (acc, it) ->
                     acc.flatMap(prevState ->
-                        DomainCommand.handleCommand(prevState, command).flatMap(evt -> RootEventHandler.handleEvent(prevState, evt))));
+                        DomainCommand.handleCommand(prevState, spielerPunkteCommand).flatMap(evt -> RootEventHandler.handleEvent(prevState, evt))));
 
         assertThat(result.isRight()).isTrue();
         result.forEach(t -> assertAll(
@@ -51,6 +67,11 @@ class ScenarioTest {
 
     }
 
+    private static Function<Tuple2<CSatz, CGame>, Either<DomainProblem, Tuple2<CSatz, CGame>>> gegnerPunktetStateTransfer(final DomainCommand command) {
+        return (Tuple2<CSatz, CGame> prevState) ->
+            DomainCommand.handleCommand(prevState, command).flatMap(evt -> RootEventHandler.handleEvent(prevState, evt));
+    }
+
     private CGame LaufendesCGameWith(final int spieler, final int gegner) {
         return CGame.of(spieler, gegner).get();
     }
@@ -59,7 +80,11 @@ class ScenarioTest {
         return Tuple.of(CSatz.zero(), CGame.zero());
     }
 
-    private static DomainCommand DomainCommandWith() {
+    private static DomainCommand GegnerPunktetWith() {
+        return new GegnerPunktet();
+    }
+
+    private static DomainCommand SpielerPunktetWith() {
         return new SpielerPunktet();
     }
 
