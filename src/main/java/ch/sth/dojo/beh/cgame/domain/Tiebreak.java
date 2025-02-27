eb01d151c577eafdfd0209bda1a3121eb64a9b82
u203243 <simon.thierstein@sbb.ch>
2025-02-27 16:42:53 +0100

    Tiebreak

--- src/main/java/ch/sth/dojo/beh/cgame/domain/Tiebreak.java
+++ src/main/java/ch/sth/dojo/beh/cgame/domain/Tiebreak.java
@@ -5,7 +5,6 @@
 package ch.sth.dojo.beh.cgame.domain;
 
 import static ch.sth.dojo.beh.PredicateUtils.compose;
-import static ch.sth.dojo.beh.PredicateUtils.eq;
 
 import ch.sth.dojo.beh.DomainProblem;
 import io.vavr.control.Either;
@@ -13,8 +12,7 @@ import java.util.function.Predicate;
 
 public record Tiebreak(TiebreakSpielerPunkteBisGame tiebreakSpielerPunkteBisGame, TiebreakGegnerPunkteBisGame tiebreakGegnerPunkteBisGame) implements CGame {
 
-    public static final Predicate<Tiebreak> passIfGegnerOnePunktBisSatz = compose(eq(1), tiebreak -> tiebreak.tiebreakGegnerPunkteBisGame.tiebreakPunkteBisGame().value());
-    public static final Predicate<Tiebreak> passIfGegnerOnePunktBisSatz = compose(TiebreakPunkteBisGame.eq1, tiebreak -> tiebreak.tiebreakGegnerPunkteBisGame.tiebreakPunkteBisGame());
+    public static final Predicate<Tiebreak> passIfGegnerOnePunktBisSatz = compose(TiebreakGegnerPunkteBisGame.eq1, Tiebreak::tiebreakGegnerPunkteBisGame);
     public static final Predicate<Tiebreak> passIfSpielerOnePunktBisSatz = compose(TiebreakSpielerPunkteBisGame.eq1, Tiebreak::tiebreakSpielerPunkteBisGame);
 
     public static Either<DomainProblem, Tiebreak> of(Integer spielerPunkteBisGame, Integer gegnerPunkteBisGame) {
--- src/main/java/ch/sth/dojo/beh/cgame/domain/TiebreakGegnerPunkteBisGame.java
+++ src/main/java/ch/sth/dojo/beh/cgame/domain/TiebreakGegnerPunkteBisGame.java
@@ -1,9 +1,13 @@
 package ch.sth.dojo.beh.cgame.domain;
 
 import ch.sth.dojo.beh.DomainProblem;
+import ch.sth.dojo.beh.PredicateUtils;
 import io.vavr.control.Either;
+import java.util.function.Predicate;
 
 public record TiebreakGegnerPunkteBisGame(TiebreakPunkteBisGame tiebreakPunkteBisGame) {
+
+    static final Predicate<TiebreakGegnerPunkteBisGame> eq1 = PredicateUtils.compose(TiebreakPunkteBisGame.eq1, TiebreakGegnerPunkteBisGame::tiebreakPunkteBisGame);
 
     static Either<DomainProblem, TiebreakGegnerPunkteBisGame> of(final Integer gegnerPunkteBisGame) {
         return TiebreakPunkteBisGame.of(gegnerPunkteBisGame)
