cdff37053641f5af232f466a3cadbd9b5eb5bce3
u203243 <simon.thierstein@sbb.ch>
2025-02-28 15:41:15 +0100

    CMatch

---
+++ src/test/java/ch/sth/dojo/beh/cmatch/CMatchTest.java
@@ -0,0 +1,27 @@
+package ch.sth.dojo.beh.cmatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.sth.dojo.beh.cmatch.domain.CMatch;
+import ch.sth.dojo.beh.evt.DomainEvent;
+import org.junit.jupiter.api.Test;
+
+class CMatchTest {
+
+    @Test
+    void name() {
+        assertThat(CMatch.handleEvent(prevMatchState(), event())).isEqualTo(nextExpectedState());
+    }
+
+    private static CMatch nextExpectedState() {
+        return null;
+    }
+
+    private static DomainEvent event() {
+        return null;
+    }
+
+    private static CMatch prevMatchState() {
+        return null;
+    }
+}
