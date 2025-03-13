6ad75050bbc003a1dd92c19628f627eb60a2036e
u203243 <simon.thierstein@sbb.ch>
2025-03-13 07:30:53 +0100

    #1 Web Connector einbauen

---
+++ src/main/java/ch/sth/dojo/beh/ReactiveWebServiceApplication.java
@@ -0,0 +1,17 @@
+package ch.sth.dojo.beh;
+
+import ch.sth.dojo.beh.infra.GreetingClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ReactiveWebServiceApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ReactiveWebServiceApplication.class, args);
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+        // We need to block for the content here or the JVM might exit before the message is logged
+        System.out.println(">> message = " + greetingClient.getMessage().block());
+    }
+}
